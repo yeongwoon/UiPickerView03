52eb6c7d1751c2a0a7d7a31b6e2f9efce202a8ea
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-14 21:10:35 +0900

    Initial Commit

---
+++ UiPickerView Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UiPickerView Test
+//
+//  Created by 김종현 on 2018. 4. 14..
+//  Copyright © 2018년 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
